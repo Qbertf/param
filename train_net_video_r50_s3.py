dabe4de00b9d300d3a2e665f4ea940765c83a500
Qbertf <christopher.nolan.ytb0@gmail.com>
2024-12-19 15:52:39 +0330

    Update train_net_video_r50_s3.py

--- train_net_video_r50_s3.py
+++ train_net_video_r50_s3.py
@@ -255,7 +255,7 @@ def setup(args):
     """
     cfg = get_cfg()
     
-    cfg.SOLVER.CHECKPOINT_PERIOD = 10
+    cfg.SOLVER.CHECKPOINT_PERIOD = 500
     # for poly lr schedule
     add_deeplab_config(cfg)
     add_maskformer2_config(cfg)
